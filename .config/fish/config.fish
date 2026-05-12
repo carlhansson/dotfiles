f2e8dfc59191902b5b53d1cacf027658873b20af
Carl Hansson <carlhansson677@gmail.com>
2026-05-12 17:21:05 +0200

    remove magic z report

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     alias code="cursor"
     alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
     alias tree="tree -C "
-    alias magic-z-report="python3 ~/repos/magic-z-reports/zreport.py"
 end
 
 set -gx REPOBEE_JUNIT4_CLASSPATH "/home/carl/.config/repobee/junit-4.13.2.jar:/home/carl/.config/repobee/hamcrest-core-1.3.jar"
