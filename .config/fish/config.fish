b82ba3af387a8d99a9356779879dbc1dc3990f2f
Carl Hansson <carlhansson677@gmail.com>
2026-07-01 22:22:39 +0200

    add editor setting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,9 @@ if status is-interactive
     alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
     alias tree="tree -C "
 end
-
+# Default Editor/Diff (for paru, pacdiff, git, etc.)
+set -gx EDITOR "nvim"
+set -gx VISUAL "nvim"
+set -gx DIFFPROG "nvim -d"
+# Repobee
 set -gx REPOBEE_JUNIT4_CLASSPATH "/home/carl/.config/repobee/junit-4.13.2.jar:/home/carl/.config/repobee/hamcrest-core-1.3.jar"
