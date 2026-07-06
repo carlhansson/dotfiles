75aacf9bb2e3c9aa4af3c5195286bbebd3031ee8
Carl Hansson <carlhansson677@gmail.com>
2026-07-07 00:11:04 +0200

    remove unnecessary stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
 if status is-interactive
-    alias code="cursor"
     alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
     alias tree="tree -C "
     set fish_greeting
@@ -10,3 +9,4 @@ set -gx VISUAL "nvim"
 set -gx DIFFPROG "nvim -d"
 # Repobee
 set -gx REPOBEE_JUNIT4_CLASSPATH "/home/carl/.config/repobee/junit-4.13.2.jar:/home/carl/.config/repobee/hamcrest-core-1.3.jar"
+COMPLETE=fish prek | source
